9941e60b56cb9635677fc9b25157adac6a6fa02e
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-04-08 18:33:49 +0200

    obtain spotID

--- src/main/java/fiji/plugin/trackmate/oneat/TrackCorrectorRunner.java
+++ src/main/java/fiji/plugin/trackmate/oneat/TrackCorrectorRunner.java
@@ -32,221 +32,229 @@ import net.imglib2.type.numeric.integer.IntType;
 import net.imglib2.util.Pair;
 import net.imglib2.util.ValuePair;
 
-public class  TrackCorrectorRunner  {
-
+public class TrackCorrectorRunner {
 
 	private final static Context context = TMUtils.getContext();
-	
-	
-	public static  ArrayList<Integer>  getSpotID (final ImgPlus< IntType > img, HashMap<Integer, ArrayList<Spot>> framespots ) {
-		
+
+	public static ArrayList<Integer> getSpotID(final Settings settings, final Model model, final ImgPlus<IntType> img,
+			HashMap<Integer, ArrayList<Spot>> framespots) {
+
 		ArrayList<Integer> SpotIDList = new ArrayList<Integer>();
-		
+		SpotCollection allspots = model.getSpots();
+
 		int ndim = img.numDimensions();
 		RandomAccess<IntType> ranac = img.randomAccess();
 		for (Map.Entry<Integer, ArrayList<Spot>> framemap : framespots.entrySet()) {
-			
-	
+
 			int frame = framemap.getKey();
 			ArrayList<Spot> spotlist = framemap.getValue();
-		    	
-			for(Spot currentspots: spotlist) {
-				
+
+			for (Spot currentspots : spotlist) {
+
 				long[] location = new long[ndim];
-				for(int d = 0; d < ndim; ++d) {
-					
+				for (int d = 0; d < ndim; ++d)
 					location[d] = (long) currentspots.getDoublePosition(d);
-					
-				}
-				
-				ranac.setPosition(frame, img.dimensionIndex( Axes.TIME ));
+
+				ranac.setPosition(frame, img.dimensionIndex(Axes.TIME));
 				ranac.setPosition(location);
 				// Get the label ID of the current interesting spot
 				int labelID = ranac.get().get();
+
+				// Now get the spot ID
+
+				final Iterable<Spot> spotsIt = allspots.iterable(frame, false);
+				for (final Spot spot : spotsIt) {
+
+					// Now we have all the spots in this frame that are a part of the track
+
+					long[] currentlocation = new long[ndim];
+					for (int d = 0; d < ndim; ++d)
+						currentlocation[d] = (long) spot.getDoublePosition(d);
+
+					ranac.setPosition(currentlocation);
+					
+					int spotlabelID = ranac.get().get();
+					
+					if (spotlabelID == labelID)
+						SpotIDList.add(spot.ID());
+				}
+
 			}
-			
-			
+
 		}
-		
-		
+
 		return SpotIDList;
 	}
-	
-	public static Pair<  Pair<SpotCollection, HashMap<Integer, ArrayList<Spot>>>, Pair<SpotCollection, HashMap<Integer, ArrayList<Spot>>>> run(final Settings settings, final Model model, final File oneatdivisionfile, final File oneatapoptosisfile) {
-		
+
+	public static Pair<Pair<SpotCollection, HashMap<Integer, ArrayList<Spot>>>, Pair<SpotCollection, HashMap<Integer, ArrayList<Spot>>>> run(
+			final Settings settings, final Model model, final File oneatdivisionfile, final File oneatapoptosisfile) {
+
 		SpotCollection divisionspots = new SpotCollection();
 		HashMap<Integer, ArrayList<Spot>> DivisionSpotListFrame = new HashMap<Integer, ArrayList<Spot>>();
-		
-		final LogService logService = context.getService( LogService.class );
-		final StatusService statusService = context.getService( StatusService.class );
-		final OptionsService optionService = context.getService( OptionsService.class );
-		
-		
+
+		final LogService logService = context.getService(LogService.class);
+		final StatusService statusService = context.getService(StatusService.class);
+		final OptionsService optionService = context.getService(OptionsService.class);
+
 		if (oneatdivisionfile != null) {
-		String line = "";
-		String cvsSplitBy = ",";
-		int count = 0;
-		
-        ArrayList<Oneatobject> DivisionSpots = new ArrayList<Oneatobject>();	
-        HashMap<Integer, ArrayList<Oneatobject>> DivisionMap = new HashMap<Integer, ArrayList<Oneatobject>>();
-		try (BufferedReader br = new BufferedReader(new FileReader(oneatdivisionfile))) {
+			String line = "";
+			String cvsSplitBy = ",";
+			int count = 0;
 
-			while ((line = br.readLine()) != null) {
+			ArrayList<Oneatobject> DivisionSpots = new ArrayList<Oneatobject>();
+			HashMap<Integer, ArrayList<Oneatobject>> DivisionMap = new HashMap<Integer, ArrayList<Oneatobject>>();
+			try (BufferedReader br = new BufferedReader(new FileReader(oneatdivisionfile))) {
 
-				// use comma as separator
-				String[] divisionspotsfile = line.split(cvsSplitBy);
+				while ((line = br.readLine()) != null) {
 
-				if (count > 0) {
+					// use comma as separator
+					String[] divisionspotsfile = line.split(cvsSplitBy);
 
-					int time = Integer.parseInt(divisionspotsfile[0]);
-					double Z = Double.parseDouble(divisionspotsfile[1]);
-					double Y = Double.parseDouble(divisionspotsfile[2]);
-					double X = Double.parseDouble(divisionspotsfile[3]);
-					double score = Double.parseDouble(divisionspotsfile[4]);
-					double size = Double.parseDouble(divisionspotsfile[5]);
-					double confidence = Double.parseDouble(divisionspotsfile[6]);
-					double angle = Double.parseDouble(divisionspotsfile[7]);
-					
-					RealPoint point = new RealPoint(X, Y, Z);
-					Oneatobject Spot = new Oneatobject(time, point, score, size, confidence, angle);
-					
-					if (DivisionMap.get(time) == null) {
-						DivisionSpots = new ArrayList<Oneatobject>();
-						DivisionMap.put(time, DivisionSpots);
-					} else
-						DivisionMap.put(time, DivisionSpots);
-					DivisionSpots.add(Spot);
+					if (count > 0) {
+
+						int time = Integer.parseInt(divisionspotsfile[0]);
+						double Z = Double.parseDouble(divisionspotsfile[1]);
+						double Y = Double.parseDouble(divisionspotsfile[2]);
+						double X = Double.parseDouble(divisionspotsfile[3]);
+						double score = Double.parseDouble(divisionspotsfile[4]);
+						double size = Double.parseDouble(divisionspotsfile[5]);
+						double confidence = Double.parseDouble(divisionspotsfile[6]);
+						double angle = Double.parseDouble(divisionspotsfile[7]);
+
+						RealPoint point = new RealPoint(X, Y, Z);
+						Oneatobject Spot = new Oneatobject(time, point, score, size, confidence, angle);
+
+						if (DivisionMap.get(time) == null) {
+							DivisionSpots = new ArrayList<Oneatobject>();
+							DivisionMap.put(time, DivisionSpots);
+						} else
+							DivisionMap.put(time, DivisionSpots);
+						DivisionSpots.add(Spot);
+					}
+					count = count + 1;
 				}
-				count = count + 1;
-			}
-		}
-
-		catch (IOException ie) {
-			ie.printStackTrace();
-		}
-		
-		
-		// Parse each component.
-		
-		final Iterator<Entry<Integer, ArrayList<Oneatobject>>> iterator = DivisionMap.entrySet().iterator();
-		
-		while (iterator.hasNext()) {
-			final Map.Entry<Integer, ArrayList<Oneatobject>> region = iterator.next();
-
-			int frame = region.getKey();
-			ArrayList<Oneatobject> currentcell = region.getValue();
-			ArrayList<Spot> currentspots = new ArrayList<Spot>();
-			for (Oneatobject cell : currentcell) {
-				final double x =  (cell.Location.getDoublePosition(0));
-				final double y =  (cell.Location.getDoublePosition(1));
-				final double z =  (cell.Location.getDoublePosition(2));
-
-				double volume = cell.size;
-				double quality = cell.size;
-				int ndims = settings.imp.getNDimensions();
-
-				final double radius = (ndims == 2) ? Math.sqrt(volume / Math.PI)
-						: Math.pow(3. * volume / (4. * Math.PI), 1. / 3.);
-
-				Spot currentspot = new Spot(x, y, z, radius, quality);
-				currentspots.add(currentspot);
-				divisionspots.add(currentspot, frame);
-				DivisionSpotListFrame.put(frame, currentspots);
 			}
 
+			catch (IOException ie) {
+				ie.printStackTrace();
+			}
+
+			// Parse each component.
+
+			final Iterator<Entry<Integer, ArrayList<Oneatobject>>> iterator = DivisionMap.entrySet().iterator();
+
+			while (iterator.hasNext()) {
+				final Map.Entry<Integer, ArrayList<Oneatobject>> region = iterator.next();
+
+				int frame = region.getKey();
+				ArrayList<Oneatobject> currentcell = region.getValue();
+				ArrayList<Spot> currentspots = new ArrayList<Spot>();
+				for (Oneatobject cell : currentcell) {
+					final double x = (cell.Location.getDoublePosition(0));
+					final double y = (cell.Location.getDoublePosition(1));
+					final double z = (cell.Location.getDoublePosition(2));
+
+					double volume = cell.size;
+					double quality = cell.size;
+					int ndims = settings.imp.getNDimensions();
+
+					final double radius = (ndims == 2) ? Math.sqrt(volume / Math.PI)
+							: Math.pow(3. * volume / (4. * Math.PI), 1. / 3.);
+
+					Spot currentspot = new Spot(x, y, z, radius, quality);
+					currentspots.add(currentspot);
+					divisionspots.add(currentspot, frame);
+					DivisionSpotListFrame.put(frame, currentspots);
+				}
+
+			}
+
 		}
-		
-		}
-		
-		
-		
-		
+
 		SpotCollection apoptosisspots = new SpotCollection();
 		HashMap<Integer, ArrayList<Spot>> ApoptosisSpotListFrame = new HashMap<Integer, ArrayList<Spot>>();
 		if (oneatapoptosisfile != null) {
-		String line = "";
-		String cvsSplitBy = ",";
-		int count = 0;
-        ArrayList<Oneatobject> ApoptosisSpots = new ArrayList<Oneatobject>();	
-        HashMap<Integer, ArrayList<Oneatobject>> ApoptosisMap = new HashMap<Integer, ArrayList<Oneatobject>>();
-		try (BufferedReader br = new BufferedReader(new FileReader(oneatapoptosisfile))) {
+			String line = "";
+			String cvsSplitBy = ",";
+			int count = 0;
+			ArrayList<Oneatobject> ApoptosisSpots = new ArrayList<Oneatobject>();
+			HashMap<Integer, ArrayList<Oneatobject>> ApoptosisMap = new HashMap<Integer, ArrayList<Oneatobject>>();
+			try (BufferedReader br = new BufferedReader(new FileReader(oneatapoptosisfile))) {
 
-			while ((line = br.readLine()) != null) {
+				while ((line = br.readLine()) != null) {
 
-				// use comma as separator
-				String[] apoptosisspotsfile = line.split(cvsSplitBy);
+					// use comma as separator
+					String[] apoptosisspotsfile = line.split(cvsSplitBy);
 
-				if (count > 0) {
+					if (count > 0) {
 
-					int time = Integer.parseInt(apoptosisspotsfile[0]);
-					double Z = Double.parseDouble(apoptosisspotsfile[1]);
-					double Y = Double.parseDouble(apoptosisspotsfile[2]);
-					double X = Double.parseDouble(apoptosisspotsfile[3]);
-					double score = Double.parseDouble(apoptosisspotsfile[4]);
-					double size = Double.parseDouble(apoptosisspotsfile[5]);
-					double confidence = Double.parseDouble(apoptosisspotsfile[6]);
-					double angle = Double.parseDouble(apoptosisspotsfile[7]);
-					
-					RealPoint point = new RealPoint(X, Y, Z);
-					Oneatobject Spot = new Oneatobject(time, point, score, size, confidence, angle);
-					
-					if (ApoptosisMap.get(time) == null) {
-						ApoptosisSpots = new ArrayList<Oneatobject>();
-						ApoptosisMap.put(time, ApoptosisSpots);
-					} else
-						ApoptosisMap.put(time, ApoptosisSpots);
-					ApoptosisSpots.add(Spot);
+						int time = Integer.parseInt(apoptosisspotsfile[0]);
+						double Z = Double.parseDouble(apoptosisspotsfile[1]);
+						double Y = Double.parseDouble(apoptosisspotsfile[2]);
+						double X = Double.parseDouble(apoptosisspotsfile[3]);
+						double score = Double.parseDouble(apoptosisspotsfile[4]);
+						double size = Double.parseDouble(apoptosisspotsfile[5]);
+						double confidence = Double.parseDouble(apoptosisspotsfile[6]);
+						double angle = Double.parseDouble(apoptosisspotsfile[7]);
+
+						RealPoint point = new RealPoint(X, Y, Z);
+						Oneatobject Spot = new Oneatobject(time, point, score, size, confidence, angle);
+
+						if (ApoptosisMap.get(time) == null) {
+							ApoptosisSpots = new ArrayList<Oneatobject>();
+							ApoptosisMap.put(time, ApoptosisSpots);
+						} else
+							ApoptosisMap.put(time, ApoptosisSpots);
+						ApoptosisSpots.add(Spot);
+					}
+					count = count + 1;
 				}
-				count = count + 1;
-			}
-		}
-
-		catch (IOException ie) {
-			ie.printStackTrace();
-		}
-		
-		
-		// Parse each component.
-		
-		final Iterator<Entry<Integer, ArrayList<Oneatobject>>> iterator = ApoptosisMap.entrySet().iterator();
-		
-		while (iterator.hasNext()) {
-			final Map.Entry<Integer, ArrayList<Oneatobject>> region = iterator.next();
-
-			int frame = region.getKey();
-			ArrayList<Oneatobject> currentcell = region.getValue();
-			ArrayList<Spot> currentspots = new ArrayList<Spot>();
-			for (Oneatobject cell : currentcell) {
-				final double x =  (cell.Location.getDoublePosition(0));
-				final double y =  (cell.Location.getDoublePosition(1));
-				final double z =  (cell.Location.getDoublePosition(2));
-
-				double volume = cell.size;
-				double quality = cell.size;
-				int ndims = settings.imp.getNDimensions();
-
-				final double radius = (ndims == 2) ? Math.sqrt(volume / Math.PI)
-						: Math.pow(3. * volume / (4. * Math.PI), 1. / 3.);
-
-				Spot currentspot = new Spot(x, y, z, radius, quality);
-				currentspots.add(currentspot);
-				apoptosisspots.add(currentspot, frame);
-				ApoptosisSpotListFrame.put(frame, currentspots);
 			}
 
-		}
-		
-		}
-		
-		Pair<SpotCollection, HashMap<Integer, ArrayList<Spot>>> DivisionPair = new ValuePair<SpotCollection, HashMap<Integer, ArrayList<Spot>>>(divisionspots, DivisionSpotListFrame);
-		
-		Pair<SpotCollection, HashMap<Integer, ArrayList<Spot>>> ApoptosisPair = new ValuePair<SpotCollection, HashMap<Integer, ArrayList<Spot>>>(apoptosisspots, ApoptosisSpotListFrame);
+			catch (IOException ie) {
+				ie.printStackTrace();
+			}
 
-		return new ValuePair<  Pair<SpotCollection, HashMap<Integer, ArrayList<Spot>>>, Pair<SpotCollection, HashMap<Integer, ArrayList<Spot>>>>(DivisionPair, ApoptosisPair);
+			// Parse each component.
+
+			final Iterator<Entry<Integer, ArrayList<Oneatobject>>> iterator = ApoptosisMap.entrySet().iterator();
+
+			while (iterator.hasNext()) {
+				final Map.Entry<Integer, ArrayList<Oneatobject>> region = iterator.next();
+
+				int frame = region.getKey();
+				ArrayList<Oneatobject> currentcell = region.getValue();
+				ArrayList<Spot> currentspots = new ArrayList<Spot>();
+				for (Oneatobject cell : currentcell) {
+					final double x = (cell.Location.getDoublePosition(0));
+					final double y = (cell.Location.getDoublePosition(1));
+					final double z = (cell.Location.getDoublePosition(2));
+
+					double volume = cell.size;
+					double quality = cell.size;
+					int ndims = settings.imp.getNDimensions();
+
+					final double radius = (ndims == 2) ? Math.sqrt(volume / Math.PI)
+							: Math.pow(3. * volume / (4. * Math.PI), 1. / 3.);
+
+					Spot currentspot = new Spot(x, y, z, radius, quality);
+					currentspots.add(currentspot);
+					apoptosisspots.add(currentspot, frame);
+					ApoptosisSpotListFrame.put(frame, currentspots);
+				}
+
+			}
+
+		}
+
+		Pair<SpotCollection, HashMap<Integer, ArrayList<Spot>>> DivisionPair = new ValuePair<SpotCollection, HashMap<Integer, ArrayList<Spot>>>(
+				divisionspots, DivisionSpotListFrame);
+
+		Pair<SpotCollection, HashMap<Integer, ArrayList<Spot>>> ApoptosisPair = new ValuePair<SpotCollection, HashMap<Integer, ArrayList<Spot>>>(
+				apoptosisspots, ApoptosisSpotListFrame);
+
+		return new ValuePair<Pair<SpotCollection, HashMap<Integer, ArrayList<Spot>>>, Pair<SpotCollection, HashMap<Integer, ArrayList<Spot>>>>(
+				DivisionPair, ApoptosisPair);
 	}
-	
-	
-	
-	
-	
+
 }
